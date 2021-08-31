396c69b6a94754505ca906e46053d5cd8ff983bd
cjconnor18 <cjallen1988@gmail.com>
2021-08-31 10:55:54 -0500

    chapter 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,38 @@ package com.company;
 
 public class Main {
 
+    // When using the java-web-dev-exercises fork
+        // had to change things to let it run:
+            // go to file-> project structure ->
+                // change sdk version to 11
+                // change project level language to 11
+
+
+    // here's a shortcut to producing the public static void main
+    // type psvm and press enter
+    // it should create a line that you see below
     public static void main(String[] args) {
-	// write your code here
+
+        // prints line to console
+        System.out.println("Hello World!!");
+        // in javascript we used console.log();
+        // in Java, we use System.out.println();
+
+        // Now uncomment the next line and run main
+        // System.out.println('Hello World!!');
+        // it should produce an error similar to the following:
+        // 21:28 java: unclosed character literal
+        // Now, remember in JavaScript we could say a string is
+        // like this 'Hello string' or "Hello string" and those would
+        // both be strings. In Java, we use double quotes("") for string
+        // types and single quotes('') for character types. Java is more
+        // strictly type, so we do not the have freedom to interchange
+        // these anymore.
+        // Now comment line 23 back out and make sure main runs without errors.
+
+        // please keep this page handy for naming conventions reference
+        // https://education.launchcode.org/java-independent-track/chapters/introduction-and-setup/naming-conventions.html
+
+
     }
 }
